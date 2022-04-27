3eb148f7d5414497bd68cf956fc081463b71c945
Shawn Thompson <plansmash@me.com>
2022-04-27 11:47:54 -0400

    working without button

--- script.js
+++ script.js
@@ -9,11 +9,24 @@ fetch('bands.json')
 	console.log('error: ' + err);
 });
 
+function getRandomArbitrary(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
 function appendData(data) {
-	var mainContainer = document.getElementById("myData");
-	data.forEach((_datum, i) => {
-		var li = document.createElement("li");
-		li.innerHTML = data[i];
-		mainContainer.appendChild(li);
-	});
+	var randomIndex = getRandomArbitrary(0, data.length - 1);
+	var spanBand = document.getElementById("randomBand");
+	spanBand.innerHTML = data[randomIndex];
+	
+	// document.getElementById('nextBand').onclick = function () {
+	// 	spanBand.innerHTML = data[randomIndex];
+	// };
+
+	document.getElementById('nextBand').addEventListener('click', () => {
+		spanBand.innerHTML = data[randomIndex + 1];
+	  });
+
+	console.log(data[randomIndex]);
 }
