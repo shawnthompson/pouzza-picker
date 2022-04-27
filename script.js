dd1994b165353ed74988f3e0e54670378e902607
Shawn Thompson <plansmash@me.com>
2022-04-27 13:36:34 -0400

    random button fix

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
+const spanBand = document.getElementById("randomBand");
+const buttonNext = document.getElementById('nextBand');
+
 fetch('bands.json')
-.then(function (response) {
-	return response.json();
-})
-.then(function (data) {
-	appendData(data);
-})
-.catch(function (err) {
-	console.log('error: ' + err);
-});
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(data) {
+		appendData(data);
+	})
+	.catch(function(err) {
+		console.log('error: ' + err);
+	});
 
 function getRandomArbitrary(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min +1)) + min;
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function appendData(data) {
 	var randomIndex = getRandomArbitrary(0, data.length - 1);
-	var spanBand = document.getElementById("randomBand");
 	spanBand.innerHTML = data[randomIndex];
-	
-	// document.getElementById('nextBand').onclick = function () {
-	// 	spanBand.innerHTML = data[randomIndex];
-	// };
 
-	document.getElementById('nextBand').addEventListener('click', () => {
-		spanBand.innerHTML = data[randomIndex + 1];
-	  });
+	buttonNext.addEventListener('click', () => {
+		var randomIndex = getRandomArbitrary(0, data.length - 1);
+		spanBand.innerHTML = data[randomIndex];
+	});
 
 	console.log(data[randomIndex]);
 }
